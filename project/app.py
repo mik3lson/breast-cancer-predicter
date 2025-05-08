a65d0c2f3a79118ae1810e23afa7c227b00a10b1
Michael Ndudi <ndudimichael06@gmail.com>
2025-05-08 13:54:00 +0100

    initial commit

---
+++ project/app.py
@@ -0,0 +1,40 @@
+import os
+from werkzeug.utils import secure_filename
+from flask import Flask, request, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from wtforms.validators import ValidationError, InputRequired
+from flask_wtf.file import FileAllowed 
+
+
+app= Flask(__name__)
+app.config['SECRET_KEY'] ='dev'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+
+class UploadImage(FlaskForm):   #sing FlaskForm to create the html form and validate the input 
+    file = FileField( validators=[InputRequired(), FileAllowed(['jpg','png','jpeg'])])
+    submit = SubmitField('Upload')
+
+@app.route('/', methods=["POST","GET"])
+def index():
+    form = UploadImage()
+    if form.validate_on_submit():
+        file = form.file.data #first grab the file 
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
+        return "file has been uploaded"
+    return render_template('upload.html', form = form)
+
+
+"""
+@app.route('/upload', methods =['POST'])
+def upload():
+    file = request.files['files']
+    file.path = os.path.join('uploads', secure_filename(file.filename))
+    file.save(file.path)
+    return render_template ('upload.html')
+"""
+
+
+if __name__=='__main__':
+    app.run(debug=True)
